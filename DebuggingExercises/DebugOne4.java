3a0b37c17c3dd5811471b74292b12d40c46ec522
ruixuan98 <ruixuan90@gmail.com>
2022-11-15 17:25:26 +0800

    Fix Debug Files

--- DebuggingExercises/DebugOne4.java
+++ DebuggingExercises/DebugOne4.java
@@ -1,8 +1,6 @@
 import javax.swing.JOptionPane;
-public class DebugOne4
-{
-  public static main(String[] args)
-  {
-     JOptionPane.showMessageDialog(null, First GUI program)!
+public class DebugOne4{
+  public static void main(String[] args){
+     JOptionPane.showMessageDialog(null, "First GUI program!");
   }
 }
